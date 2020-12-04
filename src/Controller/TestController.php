0cda9b072ecf7afd957669ae54db03e35316ddc4
Ngoc <ngocmess@gmail.com>
2020-12-04 19:46:53 +0100

    Products on view

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -2,8 +2,8 @@
 
 namespace App\Controller;
 
-
-use Symfony\Component\HttpFoundation\Response;
+use App\Repository\CategoryRepository;
+use App\Repository\ProductRepository;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -21,18 +21,9 @@ class TestController extends AbstractController
     /**
      * @Route("/test", name="test")
      */
-    /*     public function test(ProductRepository $productRepository)
+    public function test(ProductRepository $productRepository)
     {
         $products = $productRepository->findAll();
         return $this->render('test.html.twig', ['products' => $products]);
-    } */
-
-    /**
-     * @Route("/category", name="category")
-     */
-    /*     public function category(CategoryRepository $categoryRepository)
-    {
-        $categories = $categoryRepository->findAll();
-        return $this->render('test.html.twig', ['categories' => $categories]);
-    } */
+    }
 }
